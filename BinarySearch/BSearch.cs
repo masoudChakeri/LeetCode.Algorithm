86f269ecb483bd1bd65ea22266ce51259797a731
masoudChakeri <masoud.chakeri@gmail.com>
2022-11-23 12:50:11 +0330

    Add project files.

---
+++ BinarySearch/BSearch.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+    internal class BSearch
+    {
+        public int Search(int[] nums, int target, int ind = 0)
+        {
+            var result = -1;
+
+            if (nums.Length == 1)
+            {
+                if (nums[0] == target)
+                {
+                    result = ind;
+                    Console.WriteLine(result);
+                    return result;
+                }
+
+                Console.WriteLine(result);
+                return result;
+            }
+
+
+            var n = nums.Length / 2;
+
+            if (nums[n] == target)
+            {
+                result = n + ind;
+                Console.WriteLine(result);
+                return result;
+            }
+
+            if (target < nums[n])
+            {
+                return Search(nums[0..n], target, ind);
+            }
+            else if (target > nums[n])
+            {
+                ind += n;
+                return Search(nums[n..nums.Length], target, ind);
+            }
+
+            Console.WriteLine(result);
+            return result;
+        }
+    }
+}
---
+++ BinarySearch/Program.cs
@@ -0,0 +1,6 @@
+﻿using BinarySearch;
+
+var s = new BSearch();
+var nums = new int[] { -1, 0, 3, 5, 9, 12 };
+//s.Search(nums, 9);
+s.Search(nums, 9);
