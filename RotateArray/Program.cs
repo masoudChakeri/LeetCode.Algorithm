5114fd2f3236ee877640b00ecf68eb01dd9c020a
masoudChakeri <masoud.chakeri@gmail.com>
2022-12-05 11:38:57 +0330

    rotate array

---
+++ RotateArray/Program.cs
@@ -0,0 +1,12 @@
+﻿using RotateArray;
+
+var nums = new int[] { -1 };
+var k = 2;
+
+var ro = new Solution();    
+ro.Rotate(nums, k);
+
+foreach (var num in nums)
+{
+    Console.WriteLine(num);
+}
---
+++ RotateArray/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RotateArray
+{
+    internal class Solution
+    {
+        public void Rotate(int[] nums, int k)
+        {
+            if (k > nums.Length)
+            {
+                for (int i = nums.Length - 1; i < k; i++)
+                {
+                    nums.SetValue(0, i);
+                }
+            }
+
+            var rotated = new int[nums.Length];
+
+            Array.Copy(nums, nums.Length - k, rotated, 0, k);
+            Array.Copy(nums, 0, rotated, k, nums.Length - k);
+
+            Array.Copy(rotated, nums, nums.Length);
+        }
+    }
+}
