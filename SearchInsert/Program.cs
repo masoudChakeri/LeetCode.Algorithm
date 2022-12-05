228566819fb96f799ce7eeaace6ec7ecc48b7233
masoudChakeri <masoud.chakeri@gmail.com>
2022-12-05 10:19:25 +0330

    search insert

---
+++ SearchInsert/Program.cs
@@ -0,0 +1,9 @@
+﻿using SearchInsert;
+
+
+var nums = new int[] { 2, 3, 5, 6, 9 };
+var target = 7;
+
+var s = new Solution();
+
+Console.WriteLine(s.SearchInsert(nums, target));
---
+++ SearchInsert/Solution.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchInsert
+{
+    public class Solution
+    {
+        public int SearchInsert(int[] nums, int target, int ind = 0)
+        {
+    }
+}
