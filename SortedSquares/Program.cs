253448db2e5d7cbacbb2c66c0630f335f38bd176
masoudChakeri <masoud.chakeri@gmail.com>
2022-12-05 10:36:11 +0330

    Sorted Squares

---
+++ SortedSquares/Program.cs
@@ -0,0 +1,11 @@
+﻿
+using SortedSquares;
+
+var nums = new int[] { -4, -1, 0, 3, 10 };
+
+var sq = new Solution();
+
+foreach (var el in sq.SortedSquares(nums))
+{
+    Console.WriteLine(el);
+}
---
+++ SortedSquares/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortedSquares
+{
+    internal class Solution
+    {
+        public int[] SortedSquares(int[] nums)
+        {
+            var sortedArr = new int[nums.Length];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sortedArr[i] = nums[i] * nums[i];
+            }
+
+            Array.Sort(sortedArr);
+
+            return sortedArr;
+        }
+    }
+}
